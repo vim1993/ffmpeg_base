266484c5d4f55ad5d7a6a82a4d46abaa705a6469
vim1993 <vim1993@163.com>
2018-10-21 11:18:06 +0800

    mofify SDL2_ReSource_init, can play video, but have some quesion

--- src/videoPlay/videoPlayer.c
+++ src/videoPlay/videoPlayer.c
@@ -9,11 +9,11 @@
 extern "C" {
 #endif
 
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libswscale/swscale.h"
-#include "libavutil/imgutils.h"
-#include "SDL2/SDL.h"
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+#include <SDL2/SDL.h>
 
 #ifdef __cplusplus
 }
@@ -27,46 +27,56 @@ typedef enum status_e {
 static void SDL2_ReSource_deinit(void)
 {
     SDL_Quit();
-    
+
     return;
 }
 
-static status_e SDL2_ReSource_init(const SDL_Rect * pSDLRect, SDL_Window * pSDLWindow, SDL_Renderer * pSDLRender, SDL_Texture * pSDLTexture)
+static status_e SDL2_ReSource_init(const SDL_Rect * pSDLRect, SDL_Window ** pSDLWindow, SDL_Renderer ** pSDLRender, SDL_Texture ** pSDLTexture)
 {
-    if(!pSDLRect)
+    if(!pSDLRect || pSDLWindow == NULL || pSDLRender == NULL || pSDLTexture == NULL)
     {
         printf("[%s][%d]! pAVCodeCt param error\n", __func__, __LINE__);
         return STATUS_FALSE;
     }
-    
-    if(SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_TIMER) < 0)
+
+    SDL_Window * pSDLWindowt = NULL;
+    SDL_Renderer * pSDLRendert = NULL;
+    SDL_Texture * pSDLTexturet = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER))
     {
         printf("[%s][%d]! SDL_Init failed\n", __func__, __LINE__);
         return STATUS_FALSE;
     }
-    
-    pSDLWindow = SDL_CreateWindow("this is sample video player", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+
+
+
+    pSDLWindowt = SDL_CreateWindow("this is sample video player", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                     pSDLRect->w, pSDLRect->h, SDL_WINDOW_OPENGL);
-    if(!pSDLWindow)
+    if(!pSDLWindowt)
     {
         printf("[%s][%d]! SDL_CreateWindow failed\n", __func__, __LINE__);
         return STATUS_FALSE;
     }
 
-    pSDLRender = SDL_CreateRenderer(pSDLWindow, -1, 0);
-    if(!pSDLRender)
+    pSDLRendert = SDL_CreateRenderer(pSDLWindowt, -1, 0);
+    if(!pSDLRendert)
     {
         printf("[%s][%d]! SDL_CreateRenderer failed\n", __func__, __LINE__);
         return STATUS_FALSE;
     }
 
-    pSDLTexture = SDL_CreateTexture(pSDLRender, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pSDLRect->w, pSDLRect->h);
-    if(!pSDLTexture)
+    pSDLTexturet = SDL_CreateTexture(pSDLRendert, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pSDLRect->w, pSDLRect->h);
+    if(!pSDLTexturet)
     {
         printf("[%s][%d]! SDL_CreateRenderer failed\n", __func__, __LINE__);
         return STATUS_FALSE;
     }
-    
+
+    *pSDLWindow = pSDLWindowt;
+    *pSDLRender = pSDLRendert;
+    *pSDLTexture = pSDLTexturet;
+
     return STATUS_TRUE;
 }
 
@@ -81,14 +91,15 @@ status_e player_main(int argc, char * argv[])
     AVFrame * pAVframe = NULL;
     AVFrame * pAVframeYUV = NULL;
     AVPacket * pAVPacket = NULL;
+    char *out_buffer = NULL;
 
     struct SwsContext * pSwsCt = NULL;
-    
+
     SDL_Rect       SDLRect = {0};
     SDL_Window *   pSDLWindow = NULL;
     SDL_Renderer * pSDLRender = NULL;
     SDL_Texture *  pSDLTexture = NULL;
-    
+
     char * uri = "../../ts/RecordedTs_7.ts";
 
     //1. init ffmpeg, register & net init
@@ -123,7 +134,7 @@ status_e player_main(int argc, char * argv[])
     printf("[%s][%d]! find %d streams\n", __func__, __LINE__, pAVFormatCt->nb_streams);
     for(index; index < pAVFormatCt->nb_streams; index++)
     {
-        if(pAVFormatCt->streams[index]->codec->codec_type == AVMEDIA_TYPE_VIDEO) 
+        if(pAVFormatCt->streams[index]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
         {
             videoFirstIndex = index;
             printf("[%s][%d]! first video stream %d\n", __func__, __LINE__, videoFirstIndex);
@@ -171,7 +182,7 @@ status_e player_main(int argc, char * argv[])
         goto exit_3;
     }
 
-    //11. malloc YUV original data buffer 
+    //11. malloc YUV original data buffer
     if((pAVframeYUV = av_frame_alloc()) == NULL)
     {
         printf("[%s][%d]! open codec failed\n", __func__, __LINE__);
@@ -179,9 +190,13 @@ status_e player_main(int argc, char * argv[])
         goto exit_3;
     }
 
+    out_buffer =(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P,  pAVCodeCt->width, pAVCodeCt->height,1));
+    av_image_fill_arrays(pAVframeYUV->data, pAVframeYUV->linesize,out_buffer,
+            AV_PIX_FMT_YUV420P,pAVCodeCt->width, pAVCodeCt->height,1);
+
     //12. create swop context
-    pSwsCt = sws_getContext(pAVCodeCt->width, pAVCodeCt->height, pAVCodeCt->pix_fmt, 
-                                pAVCodeCt->width, pAVCodeCt->height, AV_PIX_FMT_YUV420P, 
+    pSwsCt = sws_getContext(pAVCodeCt->width, pAVCodeCt->height, pAVCodeCt->pix_fmt,
+                                pAVCodeCt->width, pAVCodeCt->height, AV_PIX_FMT_YUV420P,
                                 SWS_BICUBIC, NULL, NULL, NULL);
     if(!pSwsCt)
     {
@@ -193,27 +208,28 @@ status_e player_main(int argc, char * argv[])
     SDLRect.y = 0;
     SDLRect.w = pAVCodeCt->width;
     SDLRect.h = pAVCodeCt->height;
-    if(SDL2_ReSource_init(&SDLRect, pSDLWindow, pSDLRender, pSDLTexture) == STATUS_FALSE)
+
+    if(SDL2_ReSource_init(&SDLRect, &pSDLWindow, &pSDLRender, &pSDLTexture) == STATUS_FALSE)
     {
         goto exit_1;
     }
-    
+
     while(av_read_frame(pAVFormatCt, pAVPacket) >= 0)
     {
         if(pAVPacket->stream_index == videoFirstIndex)
         {
             i32Ret = avcodec_decode_video2(pAVCodeCt, pAVframe, &got_picture, pAVPacket);
             if(i32Ret < 0)
-            {   
+            {
                 goto exit_1;
             }
 
             if(got_picture)
             {
                 sws_scale(pSwsCt, (const uint8_t * const *)pAVframe->data, pAVframe->linesize, 0, pAVframe->height, pAVframeYUV->data, pAVframeYUV->linesize);
-                SDL_UpdateYUVTexture(pSDLTexture, &SDLRect, 
-                                        pAVframeYUV->data[0], pAVframeYUV->linesize[0], 
-                                        pAVframeYUV->data[1], pAVframeYUV->linesize[1], 
+                SDL_UpdateYUVTexture(pSDLTexture, &SDLRect,
+                                        pAVframeYUV->data[0], pAVframeYUV->linesize[0],
+                                        pAVframeYUV->data[1], pAVframeYUV->linesize[1],
                                         pAVframeYUV->data[2], pAVframeYUV->linesize[2]);
                 SDL_RenderClear(pSDLRender);
                 SDL_RenderCopy(pSDLRender, pSDLTexture, NULL, &SDLRect);
@@ -226,10 +242,10 @@ status_e player_main(int argc, char * argv[])
 
     retValue = STATUS_TRUE;
 
-exit_1:    
+exit_1:
     SDL2_ReSource_deinit();
-    sws_freeContext(pSwsCt);  
-    
+    sws_freeContext(pSwsCt);
+
 exit_2:
     av_frame_free(&pAVframe);
     av_frame_free(&pAVframeYUV);
@@ -237,7 +253,7 @@ exit_2:
 exit_3:
     av_packet_free(&pAVPacket);
     avcodec_close(pAVCodeCt);
-    
+
 exit_4:
     avformat_close_input(&pAVFormatCt);
 
