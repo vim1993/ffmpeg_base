8ac1470dc12554b1217a094b15caa35e574e75d6
zheng.yi <vim1993@163.com>
2018-10-23 09:59:59 +0800

    compiler shared lib

--- thirdparty/ffmpeg-3.2.12/build_x86.sh
+++ thirdparty/ffmpeg-3.2.12/build_x86.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./configure --enable-memalign-hack --enable-ffplay --enable-static --enable-gpl --enable-libx264 --extra-cflags=-I/usr/local/include --extra-ldflags=-L/usr/local/lib
+./configure --enable-memalign-hack --enable-ffplay --enable-shared --enable-gpl --enable-libx264 --extra-cflags=-I/usr/local/include --extra-ldflags=-L/usr/local/lib
